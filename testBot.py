fb7bab6896bdab19f0d25f9b925739387bf4f7d9
Victor Nguyen <vngu6278@uni.sydney.edu.au>
2024-12-05 21:24:19 +1100

    Test Bot for connection to Discord API purposes

---
+++ testBot.py
@@ -0,0 +1,22 @@
+import discord
+import json
+
+with open('config.json', 'r') as config:
+    configData = json.load(config)
+token = configData["token"]
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(token)
